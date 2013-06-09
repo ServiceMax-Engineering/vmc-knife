a383ba0a4141d2752a492ed84cd72b3ce9b65401
arkxu <fangzhou@arkxu.com>
2013-06-08 17:15:00 -0700

    fix chdir inside chdir warning will make exit code not 0

--- lib/vmc_knife/version.rb
+++ lib/vmc_knife/version.rb
@@ -2,7 +2,7 @@ module VMC
   module KNIFE
     module Cli
       # This version number is used as the RubyGem release version.
-      VERSION = '0.0.50'
+      VERSION = '0.0.52'
     end
   end
 end
