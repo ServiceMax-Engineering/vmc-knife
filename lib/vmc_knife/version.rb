b401a7d93d3660b3ace1d02fa7006456ea621561
arkxu <fangzhou@arkxu.com>
2014-04-28 12:22:07 -0700

    bump 0.0.60

--- lib/vmc_knife/version.rb
+++ lib/vmc_knife/version.rb
@@ -2,7 +2,7 @@ module VMC
   module KNIFE
     module Cli
       # This version number is used as the RubyGem release version.
-      VERSION = '0.0.53'
+      VERSION = '0.0.60'
     end
   end
 end
